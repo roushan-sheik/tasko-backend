7ac2d459667484bb607b4a360737561ecd701a1a
roushan-sheik <roushansheik@gmail.com>
2025-07-31 09:29:47 +0600

    Ready for Vercel deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,20 +1,23 @@
 import dotenvFlow from "dotenv-flow";
-dotenvFlow.config();
+
+if (process.env.NODE_ENV !== "production") {
+  dotenvFlow.config();
+}
 
 export default {
-  NODE_ENV: process.env.NODE_ENV,
+  NODE_ENV: process.env.NODE_ENV || "development",
   // Cors origin
-  CORS_ORIGIN: process.env.CORS_ORIGIN,
-  PORT: process.env.PORT,
-  DB_NAME: process.env.DB_NAME,
+  CORS_ORIGIN: process.env.CORS_ORIGIN || "*",
+  PORT: process.env.PORT || 5000,
+  DB_NAME: process.env.DB_NAME || "task-management",
   // mongodb connection string
   MONGO_URI: process.env.MONGO_URI,
   // bcrypt salt
-  BCRYPT_SALT: process.env.BCRYPT_JS_SALT_ROUNDS,
+  BCRYPT_SALT: process.env.BCRYPT_JS_SALT_ROUNDS || "5",
   // json webtoken secret
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
-  JWT_ACCESS_EXPIRY: process.env.JWT_ACCESS_TOKEN_EXPIRY,
+  JWT_ACCESS_EXPIRY: process.env.JWT_ACCESS_TOKEN_EXPIRY || "7d",
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET,
-  JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_TOKEN_EXPIRY,
-  GLOBAL: process.env.GlOBAL,
+  JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_TOKEN_EXPIRY || "15d",
+  GLOBAL: process.env.GLOBAL || "GLOBAL from development",
 };
